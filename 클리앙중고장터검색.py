1703cc765aef20356c1b5c3c41d5199827acda80
papasmf1 <papasmf1@gmail.com>
2023-02-16 16:52:23 +0900

    수업 완료

--- 클리앙중고장터검색.py
+++ 클리앙중고장터검색.py
@@ -9,24 +9,26 @@ hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) Appl
 for n in range(0,10):
         #클리앙의 중고장터 주소 
         data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
+        print(data)
         #웹브라우져 헤더 추가 
         req = urllib.request.Request(data, \
                                     headers = hdr)
-        req = urllib.request.Request(data)
+
         data = urllib.request.urlopen(req).read()
         page = data.decode('utf-8', 'ignore')
         soup = BeautifulSoup(page, 'html.parser')
-        list = soup.findAll('a', attrs={'class':'list_subject'})
+        list = soup.find_all('span', attrs={'data-role':'list-title-text'})
+
+        # <span class="subject_fixed" data-role="list-title-text" title="삼성 갤럭시북3 PRO i5 NT940XFG-KH51E 판매합니다.">
+        # 	삼성 갤럭시북3 PRO i5 NT940XFG-KH51E 판매합니다.
+        # </span>
 
         for item in list:
                 try:
-                        #<a class='list_subject'><span>text</span><span>text</span>
-                        span = item.contents[1]
-                        span2 = span.nextSibling.nextSibling
-                        title = span2.text 
-                        if (re.search('아이폰', title)):
+                        title = item.text 
+                        if (re.search('맥북', title)):
                                 print(title.strip())
-                                print('https://www.clien.net'  + item['href'])
+
                 except:
                         pass
         
